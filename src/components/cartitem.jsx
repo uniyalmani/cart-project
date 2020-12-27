4e60bc912cf859f1045ff1f4266d3964599ca979
uniyalmani <Ashutoshuniyal21@gmail.com>
2020-12-27 12:28:19 +0530

    adding clickevent

--- src/components/cartitem.jsx
+++ src/components/cartitem.jsx
@@ -10,7 +10,17 @@ class CartItem extends React.Component {
             img:''
         }
     }
-
+    increaseQuantity = ()=>{
+        const qty = this.state.qty;
+        this.setState({qty:qty+1});
+    }
+    decreaseQuantity = ()=>{
+        const qty = this.state.qty;
+        this.setState({qty:qty >0?qty-1:0});
+    }
+    deleteItem = ()=>{
+        console.log('delete');
+    }
     render() { 
         const { price ,title, qty} = this.state;
         return (  
@@ -20,13 +30,28 @@ class CartItem extends React.Component {
                 </div>
                 <div className = "right-block">
                     <div style = {{fontSize:25}}>{title}</div>
-                    <div style = {{color:'#777'}}>{price}</div>
-                    <div style = {{color:'#777'}}>{qty}</div>
+                    <div style = {{color:'#777'}}>Rs {price}</div>
+                    <div style = {{color:'#777'}}>Qty: {qty}</div>
                     <div className = "cart-item-actions">
                     {/*Button */}
-                    <img alt = "increase" className = "action-icons" src = "https://as2.ftcdn.net/jpg/00/70/16/29/500_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg" />
-                    <img alt = "decrease" className = "action-icons" src = "https://www.flaticon.com/svg/static/icons/svg/1828/1828899.svg" />
-                    <img alt = "delete" className = "action-icons" src = "https://as2.ftcdn.net/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" />
+                    <img
+                      alt = "increase"
+                      className = "action-icons" 
+                      src = "https://as2.ftcdn.net/jpg/00/70/16/29/500_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg"
+                      onClick = {this.increaseQuantity} 
+                       />
+                    <img
+                     alt = "decrease" 
+                     className = "action-icons" 
+                     src = "https://www.flaticon.com/svg/static/icons/svg/1828/1828899.svg" 
+                     onClick = {this.decreaseQuantity} 
+                     />
+                    <img 
+                    alt = "delete" 
+                    className = "action-icons" 
+                    src = "https://as2.ftcdn.net/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" 
+                    onClick = {this.deleteItem}
+                   />
                     </div>
                 </div>
                 </div>
